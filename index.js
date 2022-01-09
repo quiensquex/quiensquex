72917a63eaecd582c6a46fb055cfc392ef197e31
quiensquex <quiensquex@gmail.com>
2022-01-09 23:42:13 +0100

    initialize github repository

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const TOKEN = "OTAwMDg3MjgzODA4NjMyODkz.YW8Nmw.3uiVCKFMEmY3TO6lkgpLT3DSonI"
+
+const generateImage = require("./generateImage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+const welcomeChannelId = "928035745992683549"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server! Have fun!`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
